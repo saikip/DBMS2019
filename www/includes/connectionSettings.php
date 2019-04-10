a87323cbd7313b6895ca315fdb31081172e55f94
Priyam Saikia <32553757+saikip@users.noreply.github.com>
2019-04-10 16:34:27 -0400

    Initial Commit

---
+++ www/includes/connectionSettings.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Connection to Database
+*/
+
+$connection = oci_connect($username = 'psaikia',
+								  $password = 'wels@AINU14',
+								  $connection_string = '//oracle.cise.ufl.edu/orcl');
+?>
