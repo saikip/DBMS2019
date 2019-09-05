7c83bb28a8b90a84dbd24993e5b1e43507845d70
Priyam Saikia <32553757+saikip@users.noreply.github.com>
2019-09-05 15:39:54 -0400

    Update connectionSettings.php

--- www/includes/connectionSettings.php
+++ www/includes/connectionSettings.php
@@ -3,7 +3,7 @@
 Connection to Database
 */
 
-$connection = oci_connect($username = 'vibhav',
-								  $password = 'Vibhavgoel97',
+$connection = oci_connect($username = 'gatorid',
+								  $password = 'dbms pwd',
 								  $connection_string = '//oracle.cise.ufl.edu/orcl');
 ?>
